e76fa578d1e051b76f5156b4a779ba36bd40882d
Khayelihle Ngcobo <khayeh253@gmail.com>
2023-05-07 13:03:10 +0200

    Spring Boot 3 Project

---
+++ src/main/java/com/khayelihle/springboot3_spring6_hibernate/Springboot3Spring6HibernateApplication.java
@@ -0,0 +1,13 @@
+package com.khayelihle.springboot3_spring6_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3Spring6HibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3Spring6HibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khayelihle/springboot3_spring6_hibernate/Springboot3Spring6HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khayelihle.springboot3_spring6_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3Spring6HibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
