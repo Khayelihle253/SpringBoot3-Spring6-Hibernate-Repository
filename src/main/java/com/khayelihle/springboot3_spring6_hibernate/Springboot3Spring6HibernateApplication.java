50082322309ca0f09f2c7bd60b1208371f925636
Khayelihle Ngcobo <khayeh253@gmail.com>
2023-05-17 10:25:06 +0200

    making this application a command-line application using CommandLineRunner

--- src/main/java/com/khayelihle/springboot3_spring6_hibernate/Springboot3Spring6HibernateApplication.java
+++ src/main/java/com/khayelihle/springboot3_spring6_hibernate/Springboot3Spring6HibernateApplication.java
@@ -1,7 +1,9 @@
 package com.khayelihle.springboot3_spring6_hibernate;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * The default @SpringBootApplication enables:
@@ -20,6 +22,17 @@ public class Springboot3Spring6HibernateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot3Spring6HibernateApplication.class, args);
+	}
+
+	/**
+	 * creating a command-line application
+	 *  - this method will be executed after the spring beans have been loaded
+	 * @param args
+	 * @return
+	 */
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner ->{ System.out.println("Hello World"); }; //java lambda expression
 	}
 
 }
