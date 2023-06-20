101ad5ebc72f9b0f1a121a21ebdebb2c43e9e7a6
Khayelihle Ngcobo <khayeh253@gmail.com>
2023-06-20 10:57:31 +0200

    instructor and instructor detail entities/tables created

---
+++ src/main/java/com/khayelihle/springboot3_spring6_hibernate/section9_jpa_hibernate_advanced_mappings/entity/Instructor.java
@@ -0,0 +1,73 @@
+package com.khayelihle.springboot3_spring6_hibernate.section9_jpa_hibernate_advanced_mappings.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private int firstName;
+
+    @Column(name = "last_name")
+    private int lastName;
+
+    @Column(name = "email")
+    private int email;
+
+    public Instructor() {
+    }
+
+    public Instructor(int firstName, int lastName, int email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(int firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(int lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmail() {
+        return email;
+    }
+
+    public void setEmail(int email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "id=" + id +
+                ", firstName=" + firstName +
+                ", lastName=" + lastName +
+                ", email=" + email +
+                '}';
+    }
+}
